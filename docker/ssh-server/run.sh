6d33eac3c99e65155f9111d438c0c3a71b80d427
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-02-20 16:43:37 +1100

    only build if we need to build, gen keys if we don’t have them, and be a bit nicer about key management

--- docker/ssh-server/run.sh
+++ docker/ssh-server/run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-docker build -q -t ssh-server /vagrant/docker/ssh-server/ | grep 'Successfully built'
+[ -e /vagrant/docker/ssh-server/id_rsa ] || ssh-keygen -f /vagrant/docker/ssh-server/id_rsa -N ""
+NUM_IMAGES=$(docker images ssh-server | wc -l)
+if [ $NUM_IMAGES -lt 2 ]; then
+	docker build -q -t ssh-server /vagrant/docker/ssh-server/
+	IID=$(docker images ssh-server | grep -v REPOSITORY | awk '{ print $3 }')
+	docker tag ${IID} localhost:5000/ssh-server
+	docker push localhost:5000/ssh-server
+fi
 CID=$(docker run -d --expose=22 $@ ssh-server)
 IP=$(docker inspect -format '{{ .NetworkSettings.IPAddress }}' ${CID})
 IFS='.' read -ra ADDR <<< "$IP"
-storm add --id_file /vagrant/docker/ssh-server/id_rsa "ssh${ADDR[3]}" root@${IP} --o "StrictHostKeyChecking=no" --o "UserKnownHostsFile=/dev/null"
+NAME="ssh${ADDR[3]}"
+[ "$(storm search ${NAME})" != "no results found." ] && storm delete ${NAME}
+storm add --id_file /vagrant/docker/ssh-server/id_rsa ${NAME} root@${IP} --o "StrictHostKeyChecking=no" --o "UserKnownHostsFile=/dev/null"
