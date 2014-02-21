2e829eae31b211ff2ab21e953020a40213c02717
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-02-21 12:27:34 +1100

    adjusted ssh to be more robust and removed node.js and yeoman given they're now their own repository

--- docker/ssh-server/run.sh
+++ docker/ssh-server/run.sh
@@ -11,5 +11,17 @@ CID=$(docker run -d --expose=22 $@ ssh-server)
 IP=$(docker inspect -format '{{ .NetworkSettings.IPAddress }}' ${CID})
 IFS='.' read -ra ADDR <<< "$IP"
 NAME="ssh${ADDR[3]}"
-[ "$(storm search ${NAME})" != "no results found." ] && storm delete ${NAME}
-storm add --id_file /vagrant/docker/ssh-server/id_rsa ${NAME} root@${IP} --o "StrictHostKeyChecking=no" --o "UserKnownHostsFile=/dev/null"
+if [ `which storm` ]; then
+	[ "$(storm search ${NAME})" != "no results found." ] && storm delete ${NAME}
+	storm add --id_file /vagrant/docker/ssh-server/id_rsa ${NAME} root@${IP} --o "StrictHostKeyChecking=no" --o "UserKnownHostsFile=/dev/null"
+else
+	cat << DELIM
+Host ${name}
+    identityfile /vagrant/docker/ssh-server/id_rsa
+    hostname 172.17.0.5
+    user root
+    UserKnownHostsFile /dev/null
+    StrictHostKeyChecking no
+    port 22
+DELIM
+fi
