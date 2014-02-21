ea18e53147bac7fb1a35e6bdbac4f73f5a86a65c
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-02-21 14:32:16 +1100

    more robustness across aws and vbox, because of different users preconfigured

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,17 @@
 #
 # Script to install any left-overs
 
-set -e
 DEBIAN_FRONTEND=noninteractive
 
 # Add the lxc-docker package and other requirements
 wget -q -O - https://get.docker.io/gpg | apt-key add -
 echo "deb http://get.docker.io/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
 apt-get update -qq
-apt-get install -q -y --force-yes lxc-docker git ruby1.9.3 openvswitch-switch python-pip
+apt-get install -q -y --force-yes lxc-docker git ruby1.9.3 openvswitch-switch python-pip python-dev
 
-# Add vagrant to the docker group, so we don't have to sudo everything.
-gpasswd -a vagrant docker
+# Add user to the docker group, so we don't have to sudo everything.
+[ $(id -u vagrant >/dev/null 2>&1) ] && gpasswd -a vagrant docker
+[ $(id -u ubuntu >/dev/null 2>&1) ] && gpasswd -a ubuntu docker
 
 # Install docker-api for our provisioning script
 gem install docker-api
