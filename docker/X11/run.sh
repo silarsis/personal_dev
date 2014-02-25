e71f0d301cc705528f84f0458ab96867de92f0c8
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-02-25 16:53:22 +1100

    various fiddlings with X11, none of which I'm happy with

--- docker/X11/run.sh
+++ docker/X11/run.sh
@@ -10,5 +10,22 @@ if [ $NUM_IMAGES -lt 2 ]; then
 	docker push localhost:5000/${CONTAINER_NAME}
 fi
 
-docker run -d -p 5900:5900 ${CONTAINER_NAME}
+docker run -d -p 5900:22 ${CONTAINER_NAME}
 
+IP=$(docker inspect -format '{{ .NetworkSettings.IPAddress }}' ${CID})
+IFS='.' read -ra ADDR <<< "$IP"
+NAME="ssh${ADDR[3]}"
+if [ `which storm` ]; then
+	[ "$(storm search ${NAME})" != "no results found." ] && storm delete ${NAME}
+	storm add --id_file ${DIRNAME}/id_rsa ${NAME} root@${IP} --o "StrictHostKeyChecking=no" --o "UserKnownHostsFile=/dev/null"
+else
+	cat << DELIM
+Host ${name}
+    identityfile ${DIRNAME}/id_rsa
+    hostname ${IP}
+    user root
+    UserKnownHostsFile /dev/null
+    StrictHostKeyChecking no
+    port 22
+DELIM
+fi
--- docker/X11/startup.sh
+++ docker/X11/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo /etc/init.d/xdm start
-x11vnc -forever -create -nopw -noxdamage -ncache 10 -ncache_cr
+service xdm restart
+/usr/sbin/sshd -D
