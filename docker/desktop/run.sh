ed6da20576f416550062b3cddd66fbaaa30b24bd
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-03-03 15:32:01 +1100

    commit before refactor

--- docker/desktop/run.sh
+++ docker/desktop/run.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-docker run -d -p 22:5900 rogaha/docker-desktop
+DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONTAINER_NAME="$( basename ${DIRNAME} )"
+echo "Running ${CONTAINER_NAME} in ${DIRNAME}"
+
+NUM_IMAGES=$(docker images ${CONTAINER_NAME} | grep -v REPOSITORY | wc -l)
+if [ $NUM_IMAGES -lt 1 ]; then
+	docker build -q -t ${CONTAINER_NAME} ${DIRNAME}
+	IID=$(docker images ${CONTAINER_NAME} | grep -v REPOSITORY | awk '{ print $3 }')
+	docker tag ${IID} localhost:5000/${CONTAINER_NAME}
+	docker push localhost:5000/${CONTAINER_NAME}
+fi
+CID=$(docker run -d -p 5900:22 ${CONTAINER_NAME})
+
+docker logs $CID | grep '^User:'
