a830b12e89a98061dd55862f08d50fe74f8bcd01
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-03-04 10:00:32 +1100

    nicer format, so we can see what's being done

--- docker/registry/run.sh
+++ docker/registry/run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-run () { ${RUN_DOCKER} -d -p 5000:5000 -v /tmp/registry:/tmp/registry --name=registry registry ${CMD}; }
+run () {
+	echo ${RUN_DOCKER} -d -p 5000:5000 -v /tmp/registry:/tmp/registry --name=registry registry ${CMD}
+	${RUN_DOCKER} -d -p 5000:5000 -v /tmp/registry:/tmp/registry --name=registry registry ${CMD}
+}
