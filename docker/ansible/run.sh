0c0373ee65108d25609a36aa93b6e6d7d01cc582
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-03-03 15:31:20 +1100

    the start of refactoring the run script

--- docker/ansible/run.sh
+++ docker/ansible/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CONTAINER_NAME="$( basename ${DIRNAME} )"
 
-docker run -v /ansible -name DATA busybox true
-docker run -t -i -rm -volumes-from DATA -name ${CONTAINER_NAME} ${DIRNAME}
+run () {
+	[ `docker ps -a | grep DATA` ] || docker run -v /ansible -name DATA busybox true
+	docker run -t -i -rm -volumes-from DATA -name ${CONTAINER_NAME} ${CONTAINER_NAME}
+}
