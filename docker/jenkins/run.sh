668bd9983b9d0a378689636a962a4bf303172a60
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-03-04 08:58:53 +1100

    daemon by default

--- docker/jenkins/run.sh
+++ docker/jenkins/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 run () {
-	echo ${RUN_DOCKER} -p 8080:8080 -privileged michaelneale/jenkins-docker-executors ${CMD}
-	${RUN_DOCKER} -p 8080:8080 -privileged michaelneale/jenkins-docker-executors ${CMD}
+	echo ${RUN_DOCKER} -d -p 8080:8080 -privileged michaelneale/jenkins-docker-executors ${CMD}
+	${RUN_DOCKER} -d -p 8080:8080 -privileged michaelneale/jenkins-docker-executors ${CMD}
 }
