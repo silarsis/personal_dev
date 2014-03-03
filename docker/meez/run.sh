747ba85415401e01b59a2bdeeb85c2ee0ccc62d6
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-03-03 14:11:59 +1100

    proper run file for meet

--- docker/meez/run.sh
+++ docker/meez/run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-docker build -t silarsis/meez .
-docker run -i -t silarsis/meez
+DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONTAINER_NAME="$( basename ${DIRNAME} )"
+echo "Running ${CONTAINER_NAME} in ${DIRNAME}"
+
+NUM_IMAGES=$(docker images ${CONTAINER_NAME} | grep -v REPOSITORY | wc -l)
+if [ $NUM_IMAGES -lt 1 ]; then
+	docker build -q -t ${CONTAINER_NAME} ${DIRNAME}
+	IID=$(docker images ${CONTAINER_NAME} | grep -v REPOSITORY | awk '{ print $3 }')
+	docker tag ${IID} localhost:5000/${CONTAINER_NAME}
+	docker push localhost:5000/${CONTAINER_NAME}
+fi
+exec docker run -i -t ${CONTAINER_NAME}
