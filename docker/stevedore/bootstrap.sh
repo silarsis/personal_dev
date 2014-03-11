2d06a4d811f91db8e2751dcb8b18ccb688adb2ab
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-03-12 08:35:48 +1100

    removed stevedore out to it's own project

--- docker/stevedore/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Basic script to fire up a docker container with stevedore running in it
-
-docker build -q -rm -t stevedore /vagrant
-docker run -d stevedore
--- docker/stevedore/stevedore/app/controllers/github_controller.rb
+++
@@ -1,6 +0,0 @@
-class GithubController < ApplicationController
-  respond_to :json
-  def index
-  	puts JSON.parse(params[:payload]).inspect
-  end
-end
