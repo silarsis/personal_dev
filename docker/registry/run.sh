2720a523673d7566d1cdc8316c35ce40cfd83786
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-03-12 08:35:29 +1100

    squashed down to use utility method

--- docker/registry/run.sh
+++ docker/registry/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 run () {
-	echo ${RUN_DOCKER} -d -p 5000:5000 -v /tmp/registry:/tmp/registry --name=registry registry ${CMD}
-	${RUN_DOCKER} -d -p 5000:5000 -v /tmp/registry:/tmp/registry --name=registry registry ${CMD}
+	veval ${RUN_DOCKER} -d -p 5000:5000 -v /tmp/registry:/tmp/registry --name=registry registry ${CMD}
 }
