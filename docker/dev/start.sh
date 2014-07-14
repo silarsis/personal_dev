1e65c28f0cc09776eab063bd63151892657f9acb
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-07-14 12:13:11 +1000

    sort of working

--- docker/dev/start.sh
+++ docker/dev/start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-groupadd -o -g `stat -c %g /home/silarsis` silarsis
-useradd silarsis -u `stat -c %u /home/silarsis` -g silarsis -G rvm
+set +x
+echo "1"
+
+#groupadd -o -g `stat -c %g /home/silarsis` silarsis
+#useradd silarsis -u `stat -c %u /home/silarsis` -g silarsis -G rvm
+usermod -u `stat -c %u /home/silarsis/.bashrc` silarsis
+echo "2"
+usermod -g `stat -c %g /home/silarsis/.bashrc` silarsis
+echo "3"
 cd /home/silarsis
+echo "4"
 sudo -E -H -s -u silarsis /bin/bash --rcfile /opt/dev/bashrc
