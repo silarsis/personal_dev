8bb590b664c02443f99494d03127591dd25caede
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-07-14 15:08:35 +1000

    various tweaks to get things working nicely

--- docker/dev/run.sh
+++ docker/dev/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 run () {
-    ${RUN_DOCKER} -it -v /home/core/share:/home/silarsis ${CONTAINER_NAME} ${CMD}
+    ${RUN_DOCKER} -it -v /home/core/share:/home/silarsis/share -v /home/core/share/.credulous:/home/silarsis/.credulous -v /home/core/share/.ssh:/home/silarsis/.ssh ${CONTAINER_NAME} ${CMD}
 }
--- docker/dev/start.sh
+++ docker/dev/start.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 set +x
-echo "1"
 
-#groupadd -o -g `stat -c %g /home/silarsis` silarsis
-#useradd silarsis -u `stat -c %u /home/silarsis` -g silarsis -G rvm
-usermod -u `stat -c %u /home/silarsis/.bashrc` silarsis
-echo "2"
-usermod -g `stat -c %g /home/silarsis/.bashrc` silarsis
-echo "3"
+# There's a big dance here to change the user UID and GID to match that
+# of the mounted filesystem.
+
+OLD_UID=`id -u silarsis`
+OLD_GID=`id -g silarsis`
+MY_UID=`stat -c %u /home/silarsis/share/.bashrc`
+MY_GID=`stat -c %g /home/silarsis/share/.bashrc`
+MY_UMASK=`umask`
+umask 0133
+grep -v silarsis /etc/passwd > /tmp/passwd && mv /tmp/passwd /etc/passwd
+grep -v silarsis /etc/group > /tmp/group && mv /tmp/group /etc/group
+# There may be some arbitrary files that clash here (dialout group owned)
+grep -v ":${MY_GID}:" /etc/group > /tmp/group && mv /tmp/group /etc/group
+echo "silarsis:x:${MY_UID}:${MY_GID}:,,,:/home/silarsis:/bin/bash" >> /etc/passwd
+echo "silarsis:x:${MY_GID}:" >> /etc/group
 cd /home/silarsis
-echo "4"
-sudo -E -H -s -u silarsis /bin/bash --rcfile /opt/dev/bashrc
+find . -xdev -uid ${OLD_UID} | xargs chown silarsis
+find . -xdev -gid ${OLD_GID} | xargs chgrp silarsis
+umask ${MY_UMASK}
+su -l silarsis
