5dee94cde0afb581a3b37f149b68cdb295197f5c
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-07-21 08:54:36 +1000

    more tweaks to the dev environment - add the docker socket and binary, and chain the builds, and more git config

--- docker/dev/run.sh
+++ docker/dev/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+build () {
+    veval ${SOURCE} -b ${QUIETFLAG} credulous
+    veval ${BUILD_DOCKER} ${QUIETFLAG} --rm -t ${CONTAINER_NAME} ${DIRNAME}
+    veval docker tag ${CONTAINER_NAME} ${USERNAME}/${CONTAINER_NAME}
+}
+
 run () {
-    ${RUN_DOCKER} -it -v /home/core/share:/home/silarsis/share -v /home/core/share/.credulous:/home/silarsis/.credulous -v /home/core/share/.ssh:/home/silarsis/.ssh ${CONTAINER_NAME} ${CMD}
+    ${RUN_DOCKER} -it -v /home/core/share:/home/silarsis/share -v /home/core/share/.credulous:/home/silarsis/.credulous -v /home/core/share/.ssh:/home/silarsis/.ssh -v /var/run/docker.sock:/var/run/docker.sock ${CONTAINER_NAME} ${CMD}
 }
