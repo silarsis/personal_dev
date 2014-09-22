a0c4f93a706ffe6d1b9db9ea4c5f6d9d0ac65a71
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-09-22 22:28:14 +0000

    rea-aminate and rea-assuming, and X

--- docker/rea-dev/run.sh
+++ docker/rea-dev/run.sh
@@ -7,10 +7,14 @@ build () {
 }
 
 run () {
+    XVOL=${DISPLAY+"-v $(dirname $DISPLAY):$(dirname $DISPLAY)"}
+    XENV=${DISPLAY+"-e DISPLAY=$DISPLAY"}
     ${RUN_DOCKER} -it \
       -v ~:/Users/silarsis \
       -v ~/.credulous:/home/silarsis/.credulous \
+      -v ~/.rea-aminate:/home/silarsis/.rea-aminate \
+      -v ~/.rea-assuming:/home/silarsis/.rea-assuming \
       -v ~/.ssh:/home/silarsis/.ssh \
       -v /var/run/docker.sock:/var/run/docker.sock \
-      "${CONTAINER_NAME}" "${CMD}"
+      ${XVOL} ${XENV} "${CONTAINER_NAME}" "${CMD}"
 }
