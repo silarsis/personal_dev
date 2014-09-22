2b5e79c95e868c6caa1b21bf6d2c2e873bb07481
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-09-22 22:26:47 +0000

    add IDP_USER to support rea-assuming, add an alias for rea-shipper, and auto-add ssh keys

--- docker/dev/user_start.sh
+++ docker/dev/user_start.sh
@@ -5,6 +5,10 @@ export AWS_REGION=ap-southeast-2
 export AWS_USER=kevin.littlejohn
 export STACK_NAME=kevinl
 export IDP_HOST=idp.realestate.com.au
+export IDP_USER=${AWS_USER}
+
+alias ship='docker run -v $(pwd)/rea-shipper.yml:/app/rea-shipper.yml -e AWS_REGION -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SECURITY_TOKEN rea-shipper'
 
 eval $(ssh-agent -s -a /tmp/ssh-agent.sock)
+[ -d ~/.ssh/auto ] && ssh-add ~/.ssh/auto/*
 tmux
