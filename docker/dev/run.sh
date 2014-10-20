e940e3b017d18678f4b4bf86da3d83b78341b0b0
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-10-20 12:41:23 +1100

    gems container, and boot2docker portability

--- docker/dev/run.sh
+++ docker/dev/run.sh
@@ -2,15 +2,17 @@
 
 build () {
     veval docker pull "$(grep '^FROM' "${DIRNAME}"/Dockerfile | cut -d' ' -f2)"
+    veval "$0" "${QUIETFLAG}" -b gems
     veval "${BUILD_DOCKER}" "${QUIETFLAG}" --rm -t "${CONTAINER_NAME}" "${DIRNAME}"
     veval docker tag "${CONTAINER_NAME}" "${USERNAME}"/"${CONTAINER_NAME}"
 }
 
 run () {
+    veval "$0" gems ||:
     ${RUN_DOCKER} -it \
       -v ~:/Users/silarsis \
       -v ~/.ssh:/home/silarsis/.ssh \
       -v /var/run/docker.sock:/var/run/docker.sock \
-      --volumes-from rvm \
+      --volumes-from gems \
       "${CONTAINER_NAME}" "${CMD}"
 }
--- docker/dev/start.sh
+++ docker/dev/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set +x
+set -e
+set -x
 
 # There's a big dance here to change the user UID and GID to match that
 # of the mounted filesystem.
@@ -11,19 +12,26 @@ MY_UID=$(stat -c %u /Users/silarsis/.bashrc)
 MY_GID=$(stat -c %g /Users/silarsis/.bashrc)
 MY_UMASK=$(umask)
 umask 0133
-grep -v silarsis /etc/passwd > /tmp/passwd && mv /tmp/passwd /etc/passwd
-grep -v silarsis /etc/group > /tmp/group && mv /tmp/group /etc/group
+id silarsis 2>/dev/null && userdel silarsis
+getent group silarsis >/dev/null && groupdel silarsis
 # There may be some arbitrary files that clash here (dialout group owned)
-grep -v ":${MY_GID}:" /etc/group > /tmp/group && mv /tmp/group /etc/group
+#grep -v ":${MY_GID}:" /etc/group > /tmp/group && mv /tmp/group /etc/group
+getent group $MY_GID | cut -d: -f1 | xargs --no-run-if-empty groupdel
 echo "silarsis:x:${MY_UID}:${MY_GID}:,,,:/home/silarsis:/bin/bash" >> /etc/passwd
 echo "silarsis:x:${MY_GID}:" >> /etc/group
-addgroup --gid "$(stat -c %g /var/run/docker.sock)" docker
-addgroup --gid "$(stat -c %g /usr/local/rvm)" rvm
-usermod -a -G docker,rvm silarsis
+# Delete the group if it exists (boot2docker)
+DOCKER_GID=$(stat -c %g /var/run/docker.sock)
+getent group $DOCKER_GID | cut -d: -f1 | xargs --no-run-if-empty groupdel
+# Add the group and add it to the user
+addgroup --gid "$(stat -c %g /var/run/docker.sock)" host_docker
+addgroup --gid "$(stat -c %g /usr/local/lib/ruby/gems)" gems
+usermod -a -G docker,host_docker,gems silarsis
+# Link in some needed dirs and do some chowning
 cd /home/silarsis
 ln -s /Users/silarsis/git /home/silarsis/git
 ln -s /Users/silarsis/dius /home/silarsis/dius
 find . -xdev -print0 -uid "${OLD_UID}" | xargs -0 chown silarsis
 find . -xdev -print0 -gid "${OLD_GID}" | xargs -0 chgrp silarsis
 umask "${MY_UMASK}"
+# Pass things over to user land
 exec su -l -s /usr/local/bin/user_start.sh silarsis
