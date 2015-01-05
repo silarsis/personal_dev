518223e48f3911ae11eec2615f1ba0f6317ee839
Kevin Littlejohn <kevin@littlejohn.id.au>
2015-01-06 06:33:11 +1100

    changed order

--- docker/dev/start.sh
+++ docker/dev/start.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+get_current_variables() {
+  MY_UID=$(stat -c %u /Users/silarsis/.bashrc)
+  MY_GID=$(stat -c %g /Users/silarsis/.bashrc)
+  MY_UMASK=$(umask)
+  DOCKER_GID=$(stat -c %g /var/run/docker.sock)
+  RUBY_GID=$(stat -c %g /usr/local/ruby/bin/bundle)
+}
+
+delete_clashes() {
+  # Delete all in the container that matches uid or gid to external resources
+  getent group ${MY_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
+  getent group ${DOCKER_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
+  getent group ${RUBY_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
+}
+
+add_user_and_groups() {
+  addgroup --gid ${MY_GID} silarsis
+  addgroup --gid ${DOCKER_GID} host_docker
+  addgroup --gid ${RUBY_GID} ruby
+  adduser --uid ${MY_UID} --gid ${MY_GID} --gecos '' --disabled-password silarsis
+  echo 'silarsis ALL = NOPASSWD: ALL' >> /etc/sudoers
+  usermod -a -G docker,host_docker,ruby silarsis
+}
+
 configure_homedir() {
   cd /home/silarsis
   # Link in some needed dirs and do some chowning
@@ -7,29 +31,6 @@ configure_homedir() {
   ln -s /Users/silarsis/dius /home/silarsis/dius
   ln -s /Users/silarsis/.ssh /home/silarsis/.ssh
   cp /usr/local/src/bash_profile /home/silarsis/.bash_profile && chown silarsis.silarsis /home/silarsis/.bash_profile
-}
-
-get_current_variables() {
-  MY_UID=$(stat -c %u /Users/silarsis/.bashrc)
-  MY_GID=$(stat -c %g /Users/silarsis/.bashrc)
-  MY_UMASK=$(umask)
-  RUBY_GID=$(stat -c %g /usr/local/ruby/bin/bundle)
-  DOCKER_GID=$(stat -c %g /var/run/docker.sock)
-}
-
-delete_clashes() {
-  # Delete all in the container that matches uid or gid to external resources
-  getent group ${MY_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
-  getent group ${DOCKER_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
-}
-
-add_user_and_groups() {
-  addgroup --gid ${DOCKER_GID} host_docker
-  addgroup --gid ${RUBY_GID} ruby
-  addgroup --gid ${MY_GID} silarsis
-  adduser --uid ${MY_UID} --gid ${MY_GID} --gecos '' --disabled-password silarsis
-  echo 'silarsis ALL = NOPASSWD: ALL' >> /etc/sudoers
-  usermod -a -G docker,host_docker,ruby silarsis
 }
 
 set -e
