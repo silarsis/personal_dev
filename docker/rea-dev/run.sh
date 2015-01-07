41856d22a124e5d9caff33a7642f28d6b3a5a223
Kevin Littlejohn <kevin@littlejohn.id.au>
2015-01-07 04:09:14 +0000

    removed symlinks because new structure to user creation. Might want to put these back in start.sh for rea-dev specifically

--- docker/rea-dev/run.sh
+++ docker/rea-dev/run.sh
@@ -10,9 +10,6 @@ run () {
     ${RUN_DOCKER} --name rea-ruby rea-ruby ||:
     ${RUN_DOCKER} -it \
       -v ~:/Users/silarsis \
-      -v ~/.rea-aminate:/home/silarsis/.rea-aminate \
-      -v ~/.rea-assuming:/home/silarsis/.rea-assuming \
-      -v ~/.ssh:/home/silarsis/.ssh \
       -v /var/run/docker.sock:/var/run/docker.sock \
       --volumes-from rea-ruby \
       "${CONTAINER_NAME}" "${CMD}"
