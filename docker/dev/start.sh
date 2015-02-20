65dab582432e7f88667888f0b9fbbae2ad4d4b5e
Kevin Littlejohn <kevin@littlejohn.id.au>
2015-02-20 16:19:25 +1100

    factored out username and check for existence of files before doing things

--- docker/dev/start.sh
+++ docker/dev/start.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
+USERNAME="silarsis"
+
 get_current_variables() {
-  MY_UID=$(stat -c %u /Users/silarsis/.bashrc)
-  MY_GID=$(stat -c %g /Users/silarsis/.bashrc)
+  MY_UID=$(stat -c %u /Users/${USERNAME})
+  MY_GID=$(stat -c %g /Users/${USERNAME})
   MY_UMASK=$(umask)
   DOCKER_GID=$(stat -c %g /var/run/docker.sock)
-  RUBY_GID=$(stat -c %g /usr/local/ruby/bin/bundle)
+  [ -e /usr/local/ruby/bin/bundle ] && RUBY_GID=$(stat -c %g /usr/local/ruby/bin/bundle)
 }
 
 delete_clashes() {
   # Delete all in the container that matches uid or gid to external resources
   getent group ${MY_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
   getent group ${DOCKER_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
-  getent group ${RUBY_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
+  [ -e /usr/local/ruby/bin/bundle ] && getent group ${RUBY_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
 }
 
 add_user_and_groups() {
-  addgroup --gid ${MY_GID} silarsis
+  addgroup --gid ${MY_GID} ${USERNAME}
   addgroup --gid ${DOCKER_GID} host_docker
-  addgroup --gid ${RUBY_GID} ruby
-  adduser --uid ${MY_UID} --gid ${MY_GID} --gecos '' --disabled-password silarsis
-  usermod -a -G docker,host_docker,ruby silarsis
+  [ -e /usr/local/ruby/bin/bundle ] && addgroup --gid ${RUBY_GID} ruby
+  adduser --uid ${MY_UID} --gid ${MY_GID} --gecos '' --disabled-password ${USERNAME}
+  usermod -a -G docker,host_docker,ruby ${USERNAME}
 }
 
 configure_homedir() {
-  cd /home/silarsis
   # Link in some needed dirs and do some chowning
-  ln -s /Users/silarsis/git /home/silarsis/git
-  ln -s /Users/silarsis/dius /home/silarsis/dius
-  ln -s /Users/silarsis/.ssh /home/silarsis/.ssh
+  for filename in git dius .ssh; do
+    [ -e /Users/${USERNAME}/$filename ] && ln -s /Users/${USERNAME}/$filename /home/${USERNAME}/$filename
+  done
 }
 
 set -e
@@ -44,4 +45,4 @@ umask ${MY_UMASK}
 # Pass things over to user land
 set +e
 set +x
-su -l silarsis
+su -l ${USERNAME}
