6f31aeb6bf301e37b5fef8528060c6a14a6f346f
Kevin Littlejohn <kevin@littlejohn.id.au>
2015-02-22 23:54:04 +0000

    clean up the if..then handling around ruby container

--- docker/dev/start.sh
+++ docker/dev/start.sh
@@ -7,22 +7,31 @@ get_current_variables() {
   MY_GID=$(stat -c %g /Users/${USERNAME})
   MY_UMASK=$(umask)
   DOCKER_GID=$(stat -c %g /var/run/docker.sock)
-  [ -e "/usr/local/ruby/bin/bundle" ] && RUBY_GID=$(stat -c %g /usr/local/ruby/bin/bundle) ||:
+  if [ -e "/usr/local/ruby/bin/bundle" ]; then
+    RUBY_GID=$(stat -c %g /usr/local/ruby/bin/bundle)
+  fi
 }
 
 delete_clashes() {
   # Delete all in the container that matches uid or gid to external resources
   getent group ${MY_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
   getent group ${DOCKER_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
-  [ -e /usr/local/ruby/bin/bundle ] && getent group ${RUBY_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
+  if [ -e "/usr/local/ruby/bin/bundle" ]; then
+    getent group ${RUBY_GID} | cut -d: -f1 | xargs --no-run-if-empty groupdel
+  fi
 }
 
 add_user_and_groups() {
   addgroup --gid ${MY_GID} ${USERNAME}
   addgroup --gid ${DOCKER_GID} host_docker
-  [ -e /usr/local/ruby/bin/bundle ] && addgroup --gid ${RUBY_GID} ruby
+  if [ -e "/usr/local/ruby/bin/bundle" ]; then
+    addgroup --gid ${RUBY_GID} ruby
+  fi
   adduser --uid ${MY_UID} --gid ${MY_GID} --gecos '' --disabled-password ${USERNAME}
-  usermod -a -G docker,host_docker,ruby ${USERNAME}
+  usermod -a -G docker,host_docker ${USERNAME}
+  if [ -e "/usr/local/ruby/bin/bundle" ]; then
+    usermod -a -G ruby ${USERNAME}
+  fi
 }
 
 configure_homedir() {
